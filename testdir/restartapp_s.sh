a0e1da329b3131080a3e8ca532fac723184d49c1
Chi <clvrobj@gmail.com>
2012-12-11 16:54:58 +0800

    typechange

--- testdir/restartapp_s.sh
+++ testdir/restartapp_s.sh
@@ -1,1 +1,10 @@
-restartapp.sh
+#!/bin/bash
+
+PIDFILE="/var/zhangchide/zc.pid"
+
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+fi
+
+exec python ./manage.py runfcgi host=127.0.0.1 port=6000 pidfile=$PIDFILE
